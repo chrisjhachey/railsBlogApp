5787205b2c3cb48509b18be049ced99257146560
Chris Hachey <christopherjhachey@gmail.com>
2020-10-27 14:34:12 -0300

    Add 1-to-many association between Users and Articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
 
         if @article.save
             flash[:notice] = "Article was created successfully"     # this is a hash map provided by rails to help display messages
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
     # Ensure Articles without a title or description are not saved to the DB
     validates :title, presence: true, length: { minimum: 6, maximum: 100}
     validates :description, presence: true, length: { minimum: 10, maximum: 300}
+
+    belongs_to :user
 end
