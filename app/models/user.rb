37fb79194eb7276c14c877ca51f686e28fcf6e03
Chris Hachey <christopherjhachey@gmail.com>
2020-10-28 11:15:55 -0300

    Add users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # Constant
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 
-    has_many :articles    
+    has_many :articles
+
+    before_save { self.email = email.downcase }
+
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
